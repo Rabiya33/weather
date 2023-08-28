df60fe0881f2a4fb423dff187e2988476efbf236
Rabs33 <rabs@33188.com>
2023-08-28 13:21:59 +0545

    css added

--- src/Components/WeatherApp/WeatherApp.jsx
+++ src/Components/WeatherApp/WeatherApp.jsx
@@ -1,6 +1,10 @@
 import React from 'react'
 import './WeatherApp.css';
-import search from '../Assets/searchicon.png'
+import cloud from '../Assets/cloud.png'
+import humidity from '../Assets/humidity.png'
+import wind from '../Assets/wind.png'
+
+
 
 
 
@@ -10,11 +14,33 @@ const WeatherApp = () => {
       <div className='top-bar'>
         <input type='text' className='cityinput' placeholder='search'/>
         <div className="search-icon ">
-           <img src={search}  alt= ""/>
+          </div>
         </div>
+        <div className="weather-image ">
+          <img src= {cloud} alt =""/>
 
+        </div>
+        <div className="waether-temp">24°C</div>
+        <div className="weather-location">London</div>
+         <div className="data-container">
+            <div className="element">
+              <div  src={humidity} alt="" className="icon"></div>
+                <div className="data">
+                  <div className="humidity-percent">64%</div>
+                   <div className="text"> Humidity</div>
+            </div>
+          </div>
+          <div className="element">
+              <div  src={wind} alt="" className="icon"></div>
+                <div className="data">
+                  <div className="humidity-percent">18 km/h</div>
+                   <div className="text">wind speed</div>
+            </div>
+          </div>
+          
+         </div>
       </div>
-    </div>
+   
    
   )
 }
